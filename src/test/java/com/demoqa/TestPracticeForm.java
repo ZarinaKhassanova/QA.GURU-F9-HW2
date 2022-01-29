f0d5599878d94fa43dee5f58e4b12323174debee
ZarinaKhassanova <khassanova.zarisha@gmail.com>
2022-01-29 23:55:53 +0600

    Refresh tests for hw

--- src/test/java/com/demoqa/TestPracticeForm.java
+++ src/test/java/com/demoqa/TestPracticeForm.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
 
 /**
  AutomationPractice second test
@@ -22,14 +27,68 @@ public class TestPracticeForm {
 
     @BeforeEach
     void openBrowser() {
-        open("https://demoqa.com/text-box");
+        //Открываем браузер
+        open("https://demoqa.com/automation-practice-form");
     }
 
     @Test
     void testStudentRegistrationForm(){
 
-        System.out.println("Заполняем Student Registration Form");
-        $x("//div[contains(text(),'Text Box')]").shouldBe(Condition.visible);
-        System.out.println("Тест выполнен успешно");
+        //region Student Registration Form
+        //Проверяем, что страница загрузилась
+        $(byText("Student Registration Form")).shouldBe(Condition.visible, Duration.ofMillis(3000));
+        //Заполняем поле "First Name"
+        $("#firstName").setValue("Zarina");
+        //Заполняем поле "Last Name"
+        $("#lastName").setValue("Khassanova");
+        //Заполняем поле "Email"
+        $("#userEmail").setValue("test@gmail.com");
+        //Выбираем "Gender"
+        $(byText("Female")).click();
+        //Заполняем поле "Mobile"
+        $("#userNumber").setValue("7707012345");
+        // Заполняем поле "Date of Birth"
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("6");
+        $(".react-datepicker__year-select").selectOptionByValue("1984");
+        $$(".react-datepicker__day").find(text("3")).click();
+        //Заполняем поле "Subjects"
+        $("#subjectsInput").click();
+        $("#subjectsInput").sendKeys("c");
+        $(byText("Computer Science")).click();
+        //Заполняем поле "Hobbies"
+        $(byText("Sports")).click();
+        //Загружаем картинку в поле "Picture"
+        $("#uploadPicture").uploadFile(new File("src/files/octocat3.jpg"));
+        //Заполняем поле "Current Address"
+        $("#currentAddress").scrollTo();
+        $("#currentAddress").sendKeys("Goa, India");
+        //Заполняем поля "State and City"
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Panipat")).click();
+        //Кликаем на кнопку "Submit"
+        $("#submit").click();
+        //endregion
+
+        //region Check result
+        //Проверяем, что форма с таблицей открылась
+        $(byText("Thanks for submitting the form")).shouldBe(Condition.visible, Duration.ofMillis(3000));
+
+        //Проверяем данные в таблице
+        $(".table-responsive").$(byText("Zarina Khassanova"));
+        $(".table-responsive").$(byText("test@gmail.com"));
+        $(".table-responsive").$(byText("Female"));
+        $(".table-responsive").$(byText("7707012345"));
+        $(".table-responsive").$(byText("03 July,1984"));
+        $(".table-responsive").$(byText("Computer Science"));
+        $(".table-responsive").$(byText("Sports"));
+        $(".table-responsive").$(byText("octocat3.jpg"));
+        $(".table-responsive").$(byText("Goa, India"));
+        $(".table-responsive").$(byText("Haryana Panipat"));
+
+        System.out.println("Accepted form");
+        //endregion
     }
 }
